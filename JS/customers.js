20e27694404de4d21a34a5767dfb34a936d51f05
Mohamed Kamel Atlam <mohamedatlam1710@gmail.com>
2025-12-16 03:11:54 +0200

    feat: add search by name and date to customer page

--- JS/customers.js
+++ JS/customers.js
@@ -142,4 +142,108 @@ function getProducts(username) {
   return productHTML;
 }
 
+// Search ===>
+let searchMode = "name";
+
+function getSearchMode(id) {
+  let search = document.getElementById("search");
+  search.focus();
+
+  // Check ID
+  if (id == "by-name") {
+    searchMode = "name";
+  } else {
+    searchMode = "history";
+  }
+
+  // Sutup Search
+  search.placeholder = `Search By ${searchMode}`;
+  showInvoices();
+  search.value = "";
+}
+
+function searchInvoice(value) {
+  let invoicesContainer = "";
+  for (let i = 0; i < invoices.length; i++) {
+    let residual = invoices[i].total_price - invoices[i].paid;
+    // Check searchMode
+    if (searchMode == "name") {
+      // Check Char in name or not
+      if (invoices[i].name.toLowerCase().includes(value.toLowerCase())) {
+        invoicesContainer += `
+            <div class="invoices-item">
+                <div class="logo">
+                    <img src="Images/BIO_VET.jpg" alt="logo">
+                    <h2>bio vet</h2>
+                </div>
+                <div class="invoice-customer">
+                    <p id="invoice-date">${invoices[i].date}</p>
+                    <h3 id="invoice-name">${invoices[i].name}</h3>
+                </div>
+                <table border="2">
+                    <thead>
+                        <tr>
+                            <th>الصنف</th>
+                            <th>العدد</th>
+                            <th>السعر</th>
+                            <th>الاجمالي</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        ${getProducts(invoices[i].name)}
+                    </tbody>
+                </table>
+                <div class="monay">
+                    <div id="total">${invoices[i].total_price}</div>
+                    <div>
+                        <div id="pay">${invoices[i].paid}</div>
+                        <div id="residual">${residual}</div>
+                    </div>
+                </div>
+            </div>
+        `;
+      }
+    } else {
+      // Check Char in date or not
+      if (invoices[i].date.includes(value)) {
+        invoicesContainer += `
+            <div class="invoices-item">
+                <div class="logo">
+                    <img src="Images/BIO_VET.jpg" alt="logo">
+                    <h2>bio vet</h2>
+                </div>
+                <div class="invoice-customer">
+                    <p id="invoice-date">${invoices[i].date}</p>
+                    <h3 id="invoice-name">${invoices[i].name}</h3>
+                </div>
+                <table border="2">
+                    <thead>
+                        <tr>
+                            <th>الصنف</th>
+                            <th>العدد</th>
+                            <th>السعر</th>
+                            <th>الاجمالي</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        ${getProducts(invoices[i].name)}
+                    </tbody>
+                </table>
+                <div class="monay">
+                    <div id="total">${invoices[i].total_price}</div>
+                    <div>
+                        <div id="pay">${invoices[i].paid}</div>
+                        <div id="residual">${residual}</div>
+                    </div>
+                </div>
+            </div>
+        `;
+      }
+    }
+    document.querySelector(".invoices").innerHTML = invoicesContainer;
+  }
+}
+
 showInvoices();
